3f296922e486ce4540a8c94652821144240b8d2b
凯 <loyalpartner@163.com>
2020-02-02 23:53:54 +0800

    ADD: Delete workspace when exit rss/elfeed.

--- modules/private/rss/config.el
+++ modules/private/rss/config.el
@@ -15,15 +15,25 @@
       t)))
 
 ;;;###autoload
-(defun =rss (&optional inhibit-workspace)
+(defun =rss+ (&optional inhibit-workspace)
   (interactive "P")
   (+rss-setup-wconf inhibit-workspace)
   (cond ((doom-buffers-in-mode 'elfeed-search-mode (doom-buffer-list) t)
          (message "elfeed buffers are already open"))
         ((call-interactively #'elfeed))))
 
+(defun +rss/quit ()
+  (interactive)
+  (when (and (featurep! :ui workspaces)
+             (+workspace-switch +rss-workspace-name))
+    (mapc #'kill-buffer
+          (doom-buffers-in-mode '(elfeed-search-mode elfeed-show-mode)
+                                (buffer-list) t))
+    (+workspace/delete +rss-workspace-name)))
+
 (map! (:when (featurep! :app rss)
         :map elfeed-search-mode-map
         :n "gu" #'elfeed-update
-        :n "c" #'elfeed-search-clear-filter)
-      :leader "on" '=rss)
+        :n "c" #'elfeed-search-clear-filter
+        :n [remap elfeed-kill-buffer] #'+rss/quit )
+      :leader "on" '=rss+)
