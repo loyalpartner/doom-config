4035cfbce80df0e009eff1f0f17c6071e981bbe1
凯 <loyalpartner@163.com>
2020-02-02 22:05:20 +0800

    MOD: open rss use workspace.

--- modules/private/rss/config.el
+++ modules/private/rss/config.el
@@ -1,5 +1,27 @@
 ;;; private/rss/config.el -*- lexical-binding: t; -*-
 
+(defvar +rss-workspace-name "*RSS*")
+
+(defun +rss-setup-wconf (&optional inhibit-workspace)
+  (when (and (featurep! :ui workspaces)
+             (not inhibit-workspace))
+    (+workspace-switch +rss-workspace-name 'auto-create))
+  (let ((buffers (doom-buffers-in-mode 'elfeed-search-mode nil t)))
+    (if buffers
+        (ignore (switch-to-buffer (car buffers)))
+      (require 'elfeed)
+      (delete-other-windows)
+      (switch-to-buffer (doom-fallback-buffer))
+      t)))
+
+;;;###autoload
+(defun =rss (&optional inhibit-workspace)
+  (interactive "P")
+  (+rss-setup-wconf inhibit-workspace)
+  (cond ((doom-buffers-in-mode 'elfeed-search-mode (doom-buffer-list) t)
+         (message "elfeed buffers are already open"))
+        ((call-interactively #'elfeed))))
+
 (map! (:when (featurep! :app rss)
         :map elfeed-search-mode-map
         :n "gu" #'elfeed-update
