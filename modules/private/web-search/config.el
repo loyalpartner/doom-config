2354ecc99394428c657c96a3677acc42aba7be2c
凯 <loyalpartner@163.com>
2020-02-04 19:53:37 +0800

    Fix: web-search.

--- modules/private/web-search/config.el
+++ modules/private/web-search/config.el
@@ -1,11 +1,10 @@
 ;;; private/web-search/config.el -*- lexical-binding: t; -*-
 
 
-(use-package! web-search
-  :commands web-search
-  :config
-  (push '("Emacs China" "https://emacs-china.org/search?q=%s")
-        web-search-providers))
+(use-package! web-search :commands web-search)
+
+(after! web-search
+  (push '("Emacs China" "https://emacs-china.org/search?q=%s") web-search-providers))
 
 (map! :leader
       "so" #'web-search)
