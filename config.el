2b5757e7453a226fb3845618efe9e439b3b5e247
lee <loyalpartner@163.com>
2020-03-24 03:20:06 +0800

    fix linux font error.

--- config.el
+++ config.el
@@ -18,11 +18,14 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-;; test
-(setq doom-font (font-spec :family "SauceCodePro NF" :size 11)
-      ;; doom-variable-pitch-font (font-spec :family "SauceCodePro NF")
-      )
+(cond (IS-LINUX
+       (setq doom-font (font-spec :family "SauceCodePro Nerd Font" :size 15)
+             doom-variable-pitch-font (font-spec :family "SauceCodePro Nerd Font Mono")))
 
+      (IS-MAC
+       (setq doom-font (font-spec :family "SauceCodePro NF" :size 11)
+             ;; doom-variable-pitch-font (font-spec :family "SauceCodePro Nerd Font Mono")
+             )))
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. These are the defaults.
