aae9417e46e2471d6e7761cd89c0d174c8f91986
lee <loyalpartner@163.com>
2020-07-03 11:42:22 +0800

    clean code.

--- config.el
+++ config.el
@@ -172,15 +172,3 @@
        :desc "[7]" "7" 'winum-select-window-7
        :desc "[8]" "8" 'winum-select-window-8
        :desc "[9]" "9" 'winum-select-window-9))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(browse-url-browser-function (quote browse-url-chromium)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
